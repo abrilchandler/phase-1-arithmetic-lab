3d53a9fa94e1f058e4262628b2750d245f282ae3
Chandler Abril <abrilchandler1@gmail.com>
2023-08-16 18:55:40 -0700

    Work

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// Write your code here  
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() >= 0);
+
+
+const num3 = 29
+const num4 = 5
+ const mod = num3 % num4 
+
+ const max = 20
+ Math.max(17, 18, 19, 20);
